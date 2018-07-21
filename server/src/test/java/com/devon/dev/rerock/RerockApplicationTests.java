6ec8b7706073b23689bcc45aa1e5bf00c3e8f719
425devon <425devon@gmail.com>
2018-07-21 16:41:27 -0700

    initial commit

---
+++ server/src/test/java/com/devon/dev/rerock/RerockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devon.dev.rerock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RerockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
